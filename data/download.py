b18188b38d0b5d384ed9ef66c5368d8a13985ca3
peterchang77 <pchang077@gmail.com>
2020-01-23 19:33:49 -0800

    updates
    Former-commit-id: 869322e38ab31f5229972323791f68cf4ca00f2f

--- data/download.py
+++ data/download.py
@@ -1,6 +1,24 @@
+import sys
+sys.path.append('..')
+
+import argparse
 from dl_utils import datasets
 
-# =======================================================
-# datasets.download(name='bet', path='.')
-# datasets.download(name='jars', path='.')
-# =======================================================
+def download(name):
+
+    if name == 'bet':
+        datasets.download(name='bet', path='.')
+
+    if name == 'jars':
+        datasets.download(name='jars', path='.')
+
+if __name__ == '__main__':
+
+    description = 'Download test data'
+    usage = 'python download.py [-h] name'
+
+    parser = argparse.ArgumentParser(description=description, usage=usage)
+    parser.add_argument('name', metavar='name', type=str, help='name of dataset (bet, etc...)')
+    args = parser.parse_args()
+
+    download(args.name)
