ec4685d18402a223a0e9bd056b72e515308d0011
peterchang77 <pchang077@gmail.com>
2020-02-16 01:45:14 +0000

    updates

--- dl_utils/core/io/fileio.py
+++ dl_utils/core/io/fileio.py
@@ -120,14 +120,43 @@ def add_axes(data):
 
     return data 
 
-def load_json(fname, **kwargs):
+def load_json(fname, index_column=False, **kwargs):
     """
     Method to load JSON (meta-only) files
 
     """
     unique = json.load(open(fname, 'r'))
 
-    return _, {'unique': unique} 
+    # --- Attempt to load data (stored in group-label pair)
+    data = None
+    if 'arrays' in unique:
+        d = unique['arrays']
+
+        # --- Infer group
+        if 'group_' in d:
+            data = next(iter(d['group_'].values()))
+            data = np.array(data)
+
+        else:
+            DEFAULTS = ['group', 'label', 'dtype', 'fname', 'stats', 'xtags']
+            groups = [k for k in d if k not in DEFAULTS and k[:5] != '_hash']
+            if len(groups) > 0:
+                data = next(iter(d[sorted(groups)[0]].values()))
+                data = np.array(data)
+
+    # --- Format data
+    if data is not None:
+
+        if not index_column:
+            data = data[:, :-1]
+
+        if data.ndim == 2:
+            data = np.expand_dims(data, 0)
+
+        if data.ndim == 3:
+            data = np.expand_dims(data, -1)
+
+    return data, {'unique': unique} 
 
 def convert_meta_to_json_safe(meta):
 
